94e3eedb464a310dac4ade908fe939ebc40a70ab
datawine <sjx15@mails.tsinghua.edu.cn>
2017-09-08 23:14:40 +0800

    splash

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.newsapp.view.BriefInfoActivity;
+import com.example.newsapp.view.SplashActivity;
 
 /**
  * Created by junxian on 9/7/2017.
@@ -21,7 +22,7 @@ public class MainActivity extends Activity {
         //setClass函数的第一个参数是一个Context对象
         //Context是一个类,Activity是Context类的子类,也就是说,所有的Activity对象都可以向上转型为Context对象
         //setClass函数的第二个参数是Class对象,在当前场景下,应该传入需要被启动的Activity的class对象
-        intent.setClass(MainActivity.this, BriefInfoActivity.class);
+        intent.setClass(MainActivity.this, SplashActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/newsapp/view/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.newsapp.view;
+
+import android.os.Bundle;
+
+import com.example.newsapp.MainActivity;
+import com.example.newsapp.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 3000;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+        // 延迟SPLASH_DISPLAY_LENGHT时间然后跳转到MainActivity
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,
+                        BriefInfoActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
