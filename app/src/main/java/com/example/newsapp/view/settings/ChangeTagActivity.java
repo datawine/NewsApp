c4031f91fdf122f14fc9ebe0efabdce450bf45ff
datawine <sjx15@mails.tsinghua.edu.cn>
2017-09-10 16:40:44 +0800

    update category tag - add view

--- app/src/main/java/com/example/newsapp/view/settings/ChangeTagActivity.java
+++ app/src/main/java/com/example/newsapp/view/settings/ChangeTagActivity.java
@@ -1,5 +1,7 @@
 package com.example.newsapp.view.settings;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +11,7 @@ import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -24,48 +27,140 @@ import java.util.Set;
 import java.util.zip.Inflater;
 
 public class ChangeTagActivity extends AppCompatActivity {
-    private String[] mVals = new String[]
-            {"Hello", "Android", "Weclome Hi ", "Button", "TextView", "Hello",
-                    "Android", "Weclome", "Button ImageView", "TextView", "Helloworld" };
-    private ArrayList arrTab;
-    private TagAdapter<String> mTagAdapter;
-    private TagFlowLayout mTagFlowLayout;
-    private LayoutInflater mInflater;
+    private String[] mVals = {"Hello", "Android", "Weclome Hi ", "Button", "TextView", "Hello"};
+    private boolean[] mSelected = {false, false, false, false, false, false};
+    private String[] mShowVals = {};
+    private boolean[] mShowSelected = {};
+    private TagAdapter<String> mTagAdapter, mShowTagAdapter;
+    private TagFlowLayout mTagFlowLayout, mShowFlowLayout;
+    private Button btn_add, btn_delete, btn_clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_tag);
 
-        arrTab = new ArrayList();
-        //添加一条数据用于添加标签的替换
-        arrTab.add("tab");
-
+        mShowFlowLayout = (TagFlowLayout) findViewById(R.id.id_cur_flowlayout);
         mTagFlowLayout = (TagFlowLayout) findViewById(R.id.id_flowlayout);
-        mTagFlowLayout.setAdapter(new TagAdapter<String>(mVals)
+/*
+        initAdapter(mTagFlowLayout, mTagAdapter, mVals, mSelected);
+        mTagFlowLayout.setAdapter(mTagAdapter);
+        setSelectListener(mTagFlowLayout, mTagAdapter, mSelected);
+
+        initAdapter(mShowFlowLayout, mShowTagAdapter, mShowVals, mSelected);
+        mShowFlowLayout.setAdapter(mTagAdapter);
+        setSelectListener(mShowFlowLayout, mTagAdapter, mShowSelected);
+*/
+
+        initAdapter();
+        mTagFlowLayout.setAdapter(mTagAdapter);
+        setSelectListener();
+
+        btn_add = (Button) findViewById(R.id.btn_add_tag);
+        btn_delete = (Button) findViewById(R.id.btn_delete_tag);
+        btn_clear = (Button) findViewById(R.id.btn_delete_tag);
+
+        //增加的逻辑没有写去重
+        btn_add.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
+
+        btn_delete.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
+
+        btn_clear.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
+    }
+
+    public void initData(String[] str) {
+        mVals = str;
+    }
+
+    public void initAdapter() {
+        mTagAdapter = new TagAdapter<String>(mVals)
         {
             @Override
             public View getView(FlowLayout parent, int position, String s)
             {
-//                TextView tv = (TextView) LayoutInflater.from(ChangeTagActivity.this)
-//                       .inflate(R.layout.tag_textview, mTagFlowLayout, false);
-
                 RelativeLayout rl = (RelativeLayout) LayoutInflater.from(ChangeTagActivity.this)
                         .inflate(R.layout.tag_textview, mTagFlowLayout, false);
-                Log.i("finish", "finish");
+
+                if (mSelected[position])
+                    rl.setBackgroundColor(Resources.getSystem().getColor(android.R.color.holo_blue_dark));
+                else
+                    rl.setBackgroundColor(Resources.getSystem().getColor(android.R.color.holo_blue_light));
+
                 TextView tv = (TextView) rl.findViewById(R.id.tag_text);
                 tv.setText(s);
                 return rl;
             }
-        });
+        };
+    }
 
+    public void setSelectListener() {
         mTagFlowLayout.setOnSelectListener(new TagFlowLayout.OnSelectListener()
         {
             @Override
             public void onSelected(Set<Integer> selectPosSet)
             {
-                Log.i("choose:" , " choose"+ selectPosSet.toString());
+                for (int i : selectPosSet) {
+                    mSelected[i] = true;
+                }
+                mTagAdapter.notifyDataChanged();
             }
         });
     }
+
+    public void initShowAdapter() {
+        mShowTagAdapter = new TagAdapter<String>(mShowVals)
+        {
+            @Override
+            public View getView(FlowLayout parent, int position, String s)
+            {
+                RelativeLayout rl = (RelativeLayout) LayoutInflater.from(ChangeTagActivity.this)
+                        .inflate(R.layout.tag_textview, mShowFlowLayout, false);
+
+                if (mShowSelected[position])
+                    rl.setBackgroundColor(Resources.getSystem().getColor(android.R.color.holo_blue_dark));
+                else
+                    rl.setBackgroundColor(Resources.getSystem().getColor(android.R.color.holo_blue_light));
+
+                TextView tv = (TextView) rl.findViewById(R.id.tag_text);
+                tv.setText(s);
+                return rl;
+            }
+        };
+    }
+
+    public void setShowSelectListener() {
+        mShowFlowLayout.setOnSelectListener(new TagFlowLayout.OnSelectListener()
+        {
+            @Override
+            public void onSelected(Set<Integer> selectPosSet)
+            {
+                for (int i : selectPosSet) {
+                    mShowSelected[i] = true;
+                }
+                mShowTagAdapter.notifyDataChanged();
+            }
+        });
+    }
+
 }
